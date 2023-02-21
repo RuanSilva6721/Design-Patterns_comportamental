7532c957315eb9963bc3e7949a51059923f84159
Ruan Silva <ruansilva6721@gmail.com>
2023-02-21 20:03:34 -0300

    adicionando Dp 'Stage'

--- src/Orcamento.php
+++ src/Orcamento.php
@@ -1,7 +1,28 @@
 <?php
 namespace Ruan\DP;
 
+use Ruan\DP\Stage\EmAprovacao;
+use Ruan\DP\Stage\EstadoOrcamento;
+
 class Orcamento {
     public float $valor;
     public int $qtdItens;
+
+    public EstadoOrcamento $estadoAtual ;
+
+    public function __construct(){
+        $this->estadoAtual =  new EmAprovacao();
+    }
+    public function aplicaDescExtra(){
+        $this->valor -= $this->estadoAtual->calculaDescExtra($this);
+    }
+    public function aprova(){
+        $this->estadoAtual->aprova($this);
+    }
+    public function reprova(){
+        $this->estadoAtual->reprova($this);
+    }
+    public function finaliza(){
+        $this->estadoAtual->finaliza($this);
+    }
 }
